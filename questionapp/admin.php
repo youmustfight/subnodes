e237bd3839aec3bfe58740ad1de026dd2ac39f67
panayotis antoniadis <panayotis@nethood.org>
2014-12-27 19:10:38 +0100

    question app

---
+++ questionapp/admin.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html 
+     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Poll Test</title>
+<link href="poll/template/styles.css" rel="stylesheet" type="text/css" />
+<script src="/js/jquery.min.js"></script>
+</head>
+<body>
+
+<?php
+
+$question_file = "poll_data.txt";
+$question = json_decode(file_get_contents($question_file), true);
+
+if ($_GET['ssid'] && $_GET['question'] && $_GET['answers'])
+{
+    $question['ssid'] = $_GET['ssid'];
+    $question['question'] = $_GET['question'];
+    $question['answers'] = $_GET['answers'];
+
+    file_put_contents($question_file, json_encode($question));
+
+    echo exec('whoami');
+
+    exec("sudo sudo sed -i \"s/\(ssid *= *\).*/\\1".$question['ssid']."/\" /diynet/rpi_www/question/test.conf");
+    echo "running sudo sed -i \"s/\(ssid *= *\).*/\\1".$question['ssid']."/\" /var/www/html/diynet/rpi_www/question/test.conf";
+
+}
+
+
+?> 
+
+<form method=GET>
+<input name=ssid type=text size=32 value="Type here the SSID name (up to 32 characters)"></input>
+<br/>
+<br/>
+
+<input name=question type=text size=50 value="Type here your preferred question"></input>
+<br/>
+<br/>
+
+<input name=answers type=text size=100 value='Type the possible answers, delimited with ";", like "Answer 1;Answer2;...'</input>
+<br/>
+<br/>
+
+<input type=submit value=Go!></input>
+</form>
+
+</body>
+</html>
